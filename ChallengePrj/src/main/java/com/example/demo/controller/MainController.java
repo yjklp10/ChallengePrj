e3b5ac7245ec8da28b4dc293a4838e8597a88303
bluenote00 <god0937@gmail.com>
2023-01-04 09:11:22 +0900

    23/01/04

---
+++ ChallengePrj/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	
+	@RequestMapping("/home_main")
+	public String home_main() {
+		return "home_main";
+	}
+
+}
