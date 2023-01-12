3bb7d7456c75c15167a6141b9b7059cbf53f3e41
HoJun <67477544+yjklp10@users.noreply.github.com>
2023-01-12 17:26:31 +0900

    2023-01-12 별점기능 성공, 카카오맵 넣기

--- ChallengePrj/src/main/resources/static/js/reply.js
+++ ChallengePrj/src/main/resources/static/js/reply.js
@@ -5,6 +5,7 @@ Rating.prototype.setRate = function(newrate){
     //별점 마킹 - 클릭한 별 이하 모든 별 체크 처리
     this.rate = newrate;
     let items = document.querySelectorAll('.rate_radio');
+    
     items.forEach(function(item, idx){
         if(idx < newrate){
             item.checked = true;
@@ -17,6 +18,7 @@ let rating = new Rating(); //별점 인스턴스 생성
 document.addEventListener('DOMContentLoaded', function(){
     //별점선택 이벤트 리스너
     document.querySelector('.rating').addEventListener('click',function(e){
+    	
         let elem = e.target;
         if(elem.classList.contains('rate_radio')){
             rating.setRate(parseInt(elem.value));
